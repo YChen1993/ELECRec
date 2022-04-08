1a0de7814b168692b2ab699ca24680f79cd3bb29
YChen1993 <yongjun.chen@salesforce.com>
2022-04-08 00:29:47 -0700

    added copyright related files

--- src/trainers.py
+++ src/trainers.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright (c) 2022 salesforce.com, inc.
+# All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+#
 
 import numpy as np
 from tqdm import tqdm
